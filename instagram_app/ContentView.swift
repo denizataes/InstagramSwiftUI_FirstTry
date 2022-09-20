429acd59e2e0224c68a1fe30674f561e1b1016d5
Deniz Ata ES <denizataes@hotmail.com>
2022-09-21 02:52:33 +0300

    edit

--- instagram_app/ContentView.swift
+++ instagram_app/ContentView.swift
@@ -9,18 +9,88 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        
+        VStack{
+            
+            Header()
+            
+            Profile()
+            Spacer()
+            
         }
-        .padding()
+      
+        
     }
 }
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+    }
+}
+
+struct Header: View {
+    var body: some View {
+        HStack{
+            Text("denizataes")
+                .fontWeight(.bold)
+                .padding(.horizontal, 5)
+                .font(.title)
+            Spacer()
+            HStack(spacing: 12.0){
+                Image("Create")
+                Image("points")
+            }
+           
+        }
+        .padding()
+    }
+}
+
+struct Profile: View {
+    var body: some View {
+        HStack{
+            VStack(spacing: 5.0){
+                Image("ata")
+                    .resizable()
+                    .frame(width: 90,height: 90)
+                    .cornerRadius(60)
+                VStack(spacing: 2.0){
+                    Text("Deniz Ata EŞ")
+                        .font(.subheadline)
+                        .fontWeight(.bold)
+                    Text("denizataes.com")
+                        .font(.subheadline)
+                        .foregroundColor(.accentColor)
+                }
+             
+                
+            }.padding()
+            Spacer()
+            
+            HStack(spacing: 15.0){
+                VStack(){
+                    Text("8")
+                        .fontWeight(.bold)
+                    Text("Gönderiler")
+                        .foregroundColor(.secondary)
+                }
+                VStack{
+                    Text("542")
+                        .fontWeight(.bold)
+                    Text("Takipçi")
+                        .foregroundColor(.secondary)
+                }
+                VStack{
+                    Text("418")
+                        .fontWeight(.bold)
+                    Text("Takip")
+                        .foregroundColor(.secondary)
+                }
+            }
+            .padding(.horizontal,20)
+            
+            
+        }
     }
 }
