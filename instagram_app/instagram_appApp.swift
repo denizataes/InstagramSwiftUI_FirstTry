b6a592121da9894e65fcf6cee580278e12e86c91
Deniz Ata ES <denizataes@hotmail.com>
2022-09-21 01:49:06 +0300

    Initial Commit

---
+++ instagram_app/instagram_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  instagram_appApp.swift
+//  instagram_app
+//
+//  Created by Deniz Ata Eş on 21.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct instagram_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
